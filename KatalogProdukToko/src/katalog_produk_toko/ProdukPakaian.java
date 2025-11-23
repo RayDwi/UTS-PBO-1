5ec0d73d21a9dc8fa1caa316b983e7b63e449ced
Rayhan Dwi Saputra <rayhansaputra454@gmail.com>
2025-11-23 22:45:54 +0700

    Initial commit

---
+++ KatalogProdukToko/src/katalog_produk_toko/ProdukPakaian.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package katalog_produk_toko;
+
+/**
+ *
+ * @author ACER
+ */
+public class ProdukPakaian {
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UTS PBO 1
+UTS Pemrograman Berorientasi Objek 1 Rayhan Dwi Saputra
