618170ec4b2c8391e04024533f40046bd26a7fcc
Rayhan Dwi Saputra <rayhansaputra454@gmail.com>
2025-11-23 22:56:38 +0700

    Implement ProdukPakaian class with attributes and methods

--- KatalogProdukToko/src/katalog_produk_toko/ProdukPakaian.java
+++ KatalogProdukToko/src/katalog_produk_toko/ProdukPakaian.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package katalog_produk_toko;
 
-/**
- *
- * @author ACER
- */
-public class ProdukPakaian {
-    
+public class ProdukPakaian extends Produk {
+    private String ukuran;
+    private String bahan;
+
+    public ProdukPakaian(String kodeProduk, String namaProduk, double harga, String ukuran, String bahan) {
+        super(kodeProduk, namaProduk, harga);
+        this.ukuran = ukuran;
+        this.bahan = bahan;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public String getBahan() {
+        return bahan;
+    }
+
+    public String cekKategoriUkuran() {
+        if (ukuran.equalsIgnoreCase("S") || ukuran.equalsIgnoreCase("M") || ukuran.equalsIgnoreCase("L")) {
+            return "Dewasa";
+        } else {
+            return "Anak-anak";
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Ukuran: " + ukuran);
+        System.out.println("Bahan: " + bahan);
+        System.out.println("Kategori: " + cekKategoriUkuran());
+    }
 }
