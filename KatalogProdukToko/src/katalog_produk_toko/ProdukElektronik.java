2bc27c3c8f27407a98c676b9e99b57956155281d
Rayhan Dwi Saputra <rayhansaputra454@gmail.com>
2025-11-23 22:55:33 +0700

    Refactor ProdukElektronik class with new features

--- KatalogProdukToko/src/katalog_produk_toko/ProdukElektronik.java
+++ KatalogProdukToko/src/katalog_produk_toko/ProdukElektronik.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package katalog_produk_toko;
 
-/**
- *
- * @author ACER
- */
-public class ProdukElektronik {
+public class ProdukElektronik extends Produk {
+    private String merek;
+    private int garansiBulan;
     
+    public ProdukElektronik(String kodeProduk, String namaProduk, double harga, String merek, int garansiBulan) {
+        super(kodeProduk, namaProduk, harga);
+        this.merek = merek;
+        this.garansiBulan = garansiBulan;
+    }
+
+    public String getMerek() {
+        return merek;
+    }
+
+    public int getGaransiBulan() {
+        return garansiBulan;
+    }
+
+    public double hitungBiayaAsuransi() {
+        return getHarga() * 0.01;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Merek: " + merek);
+        System.out.println("Garansi: " + garansiBulan + " bulan");
+        System.out.println("Biaya Asuransi: Rp" + hitungBiayaAsuransi());
+    }
 }
